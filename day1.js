30cf03a5698f5c1440c04fbb07dd3e0a03d49dde
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-04-13 23:48:01 +0530

    initial commit

---
+++ day1.js
@@ -0,0 +1,95 @@
+// console.log("day 1 Restart")
+
+
+// let name = "Rahamat Shaikh";
+// const age = 22;
+// let isDeveloper = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(isDeveloper);
+
+
+// function 
+// function greet() {
+//     console.log("Hello, welcome back!")
+// }
+// greet();
+
+
+
+// function greetUser(name) {
+//     console.log(`Welcome ${name}`)
+// }
+// greetUser("Rahamat");
+
+
+
+// function add(a, b) {
+//     return a + b
+// }
+// console.log(add(2, 3));
+
+
+// Array and Loop
+// let number = [1,2,3,4,5];
+// for(let i = 0; i < number.length; i++){
+// console.log(number[i])
+// }
+
+
+// print only even number 
+// let numbers = [10, 29, 20, 30];
+// for(let i = 0; i< numbers.length; i++){
+//     if(numbers[i] % 2  === 0 ) {
+//         console.log(numbers[i])
+//     }
+// }
+
+
+
+// let num = [2, 3, 4, 5];
+// num.map(num => console.log(num ));
+
+
+//basic object 
+
+// let user = {
+//     name : "Rahamat",
+//     age : 26,
+//     isDeveloper : true,
+//     city : "Kolkata"
+// }
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.city);
+
+
+
+// let users = {
+//     name : "Rahamat",
+//     age : 26,
+//     isDeveloper : true,
+//     city : "Kolkata"
+// }
+// for(let key in users) {
+//     console.log(user[key]);
+// }
+
+
+// let userInfo = [
+//     {name: "Rahamat", age: 26},
+//     {name: "ram", age: 43},
+//     {name: "sam", age: 74}
+// ];
+
+// for(let key in userInfo) {
+//     if(userInfo[key].age > 27) {
+//         console.log(userInfo[key].age)
+//     }
+// }
+
+// for(let i=0; i<userInfo.length; i++) {
+//     console.log(userInfo[i].name);
+// }
+
